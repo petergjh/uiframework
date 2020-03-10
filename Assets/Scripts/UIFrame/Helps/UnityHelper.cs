f08fee5cdf6a9694e3b5a98b4e6c569f60a4f043
petergao <306739889@qq.com>
2020-03-10 15:31:39 +0800

    获取子节点的脚本

--- Assets/Scripts/UIFrame/Helps/UnityHelper.cs
+++ Assets/Scripts/UIFrame/Helps/UnityHelper.cs
@@ -6,9 +6,18 @@ using UnityEngine;
 namespace UIFrame
 {
 
+    /// <summary>
+    /// 定义帮助类
+    /// </summary>
     public class UnityHelper: MonoBehaviour
     {
          
+        /// <summary>
+        /// 递归查找子节点方法
+        /// </summary>
+        /// <param name="goParent"></param>
+        /// <param name="childName"></param>
+        /// <returns></returns>
         public static Transform FindTheChildNode(GameObject goParent,string childName)
         {
             Transform searchTrans = null;  // 查找结果
@@ -27,6 +36,43 @@ namespace UIFrame
             }
             return searchTrans;
         }
+
+
+        /// <summary>
+        /// 获取子节点脚本
+        /// </summary>
+        /// <typeparam name="T">泛型，这里代指unity组件</typeparam>
+        /// <param name="goParent"></param>
+        /// <param name="childName"></param>
+        /// <returns></returns>
+        public static T GetTheChildNodeComponentScripts<T>(GameObject goParent, string childName) where T:Component
+        {
+            Transform searchTransformNode = null;
+            searchTransformNode = FindTheChildNode(goParent, childName);
+            if(searchTransformNode != null)
+            {
+                Debug.Log(" 查找并返回子节点脚本：" + childName);
+                return searchTransformNode.gameObject.GetComponent<T>();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //给子节点添加脚本
+        public static T AddChildNodeComponent<T>(GameObject goParent, string childName) where T:Component
+        {
+            //  查找特定子节点
+
+            // 如果查找成功，比较若有同名重复脚本则删除，无测添加
+
+            // 如果查找不成功，返回null
+
+        }
+
+
+
     }
 
 }
