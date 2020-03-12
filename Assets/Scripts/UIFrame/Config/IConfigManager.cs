7c4598d6b252b433a82f979893f15ff28cbd9012
peter <306739889@qq.com>
2020-03-12 08:27:48 +0800

    iconfigManager

---
+++ Assets/Scripts/UIFrame/Config/IConfigManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 框架核心参数
+/// 为了规整，把一些全局性的常量统一放在这里，包括以下：
+/// 1. 系统常量
+/// 2. 全局方法
+/// 3. 系统枚举类型
+/// 4. 委托定义
+/// </summary>
+
+namespace UIFrame
+{
+    public interface IConfigManager
+    {
+        /// <summary>
+        /// 只读属性：应用设置通用接口
+        /// 接口功能：得到键值对集合数据
+        /// </summary>
+        Dictionary<string, string> AppSetting { get; }
+
+        // 方法：得到配置文件AppSetting最大的数量
+        int GetAppSettingMaxNumber();
+
+
+    }
+
+    
+    internal class KeyValuesInfo
+    {
+        // 配置信息（键值对集合)
+        public List<KeyValueNode> ConfigInfo = null;
+    }
+
+    internal class KeyValueNode
+    {
+        // 键
+        public string key = null;
+        // 值
+        public string value = null;
+    }
+
+
+}
